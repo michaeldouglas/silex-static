271fd999b9f746b00dc7be2594e3b653f46eb7a0
Atitude <atitude@Mac-mini-de-Atitude-2.local>
2016-09-30 11:02:56 -0300

    Ajuste da estrutura do projeto

--- web/app/tasks/tasks.js
+++ web/app/tasks/tasks.js
@@ -1,6 +1,11 @@
 var Tasks = {
     gulp: require('gulp'),
     plugins: require('gulp-load-plugins')(),
+    basePaths: {
+        sass: './app/assets/**/*.{sass,scss}',
+        js: './app/assets/**/*.js',
+        img: './app/assets/*'
+    },
     getTask: function (path, task)
     {
         return require(`./${path}/gulp${task}.js`)(this.gulp, this.plugins);
--- web/gulpfile.js
+++ web/gulpfile.js
@@ -4,9 +4,13 @@ var tasks = require('./app/tasks/tasks');
 
 // Tarefas variadas do nosso projeto
 gulp.task('sass', tasks.getTask('sass', 'Sass'));
+gulp.task('js', tasks.getTask('js', 'Js'));
+gulp.task('img', tasks.getTask('img', 'Img'));
 
 
 // Tarefa default
-gulp.task('default', ['sass'], function () {
-    gulp.watch('./assets/src/**/*.{sass,scss}', ['sass']);
+gulp.task('default', ['sass', 'js', 'img'], function () {
+    gulp.watch(tasks.basePaths.sass, ['sass']);
+    gulp.watch(tasks.basePaths.js, ['js']);
+    gulp.watch(tasks.basePaths.img, ['img']);
 });
